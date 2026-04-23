83e67a623b1805a24b25fb36da5bdcd7ad867976
exauking2006 <exaukingwina509@gmail.com>
2026-04-23 21:03:58 +0200

    Create Register.jsx

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div className="auth-container">
+      <div className="auth-box">
+        <h2>Créer un compte</h2>
+
+        <button>🔵 S'inscrire avec Google</button>
+      </div>
+    </div>
+  );
+}
