b93fda39dcf376654c7dbc2623c400fbeab35b6d
exauking2006 <exaukingwina509@gmail.com>
2026-04-23 21:15:03 +0200

    Create Maintenance.jsx

---
+++ client/src/pages/Maintenance.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Maintenance() {
+  return (
+    <div>
+      <h2>Maintenance</h2>
+      <p>Le site est en amélioration. Revenez dans 1h.</p>
+    </div>
+  );
+}
