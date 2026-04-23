cfe0d8ede3665e7b690fe678edf363d4a17f509a
octavio wina <bchat359@gmail.com>
2026-04-23 20:01:58 +0100

    Create Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="auth-container">
+      <div className="auth-box">
+        <h2>Connexion</h2>
+
+        <button>🔵 Continuer avec Google</button>
+      </div>
+    </div>
+  );
+  }
