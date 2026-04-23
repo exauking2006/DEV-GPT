b97454bfe4127b7f3fae044294a4c98c3f45c63c
octavio wina <bchat359@gmail.com>
2026-04-23 23:54:23 +0100

    Create db.js

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("🟢 MongoDB connecté");
+  } catch (error) {
+    console.error("🔴 Erreur MongoDB:", error);
+    process.exit(1);
+  }
+};
