d592d3944bd3c300ff0edf4f5714af35a6e3413e
octavio wina <bchat359@gmail.com>
2026-04-24 01:02:56 +0100

    Create report.js

---
+++ server/routes/report.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  createReport,
+  getReports,
+} from "../controllers/reportController.js";
+
+const router = express.Router();
+
+router.post("/", createReport);
+router.get("/", getReports);
+
+export default router;
